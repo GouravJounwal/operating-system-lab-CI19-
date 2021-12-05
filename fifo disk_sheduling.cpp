7ee519076e61ab068f684921c35c515e6f06eb84
GouravJounwal <76249754+GouravJounwal@users.noreply.github.com>
2021-12-05 20:26:33 +0530

    Update fifo disk_sheduling.cpp

--- fifo disk_sheduling.cpp
+++ fifo disk_sheduling.cpp
@@ -1,98 +1,39 @@
-#include <iostream>
-#include <cmath>
+#include<bits/stdc++.h>
 using namespace std;
+int main(){
 
-int main()
-{
-cout<<"Disk Scheduling\n";
-int ar[8]={65,5,14,89,20,90,72,7},n=8;
-int cur=53,d,td=0,i;
-cout<<"\n1. FCFS:";
-for(i=0;i<n;i++)
-{
-d=abs(cur-ar[i]);
-td=td+d;
-cur=ar[i];
-}
-cout<<"\tTotal Distance: "<<td;
-cout<<"\n2. SSTF:";
-int br[9]={5,7,14,20,53,65,72,89,90},l,r,j,pre,nex,li,ri;
-td=0;
-int k=4;
-for(i=0;i<n;i++)
-{
-for(j=k-1;j>=0;j--)
-{
-if(br[j]>0)
-    {pre=br[j];
-    li=j;
-    break;}
-}
-for(j=k+1;j<=9;j++)
-{
-if(k==8)
-    {
-        nex=1000;
-        break;}
-else
-    if(br[j]>0)
-    {nex=br[j];
-    ri=j;
-    break;}
-}
-l=abs(br[k]-pre);
-r=abs(nex-br[k]);
-br[k]=-1;
-if(l<r)
-    {k=li;
-    d=l;}
-else
-    {k=ri;
-    d=r;}
-td=td+d;
-}
-cout<<"\tTotal Distance: "<<td;
-cout<<"\n3. SCAN: ";
-int cr[9]={5,7,14,20,53,65,72,89,90};
-k=4;
-l=abs(cr[k]-cr[k-1]);
-r=abs(cr[k+1]-cr[k]);
-if(l<r)
-    td=cr[k]+199;
-else
-    td=199+(199-cr[k]);
-cout<<"\tTotal Distance: "<<td;
-cout<<"\n4. CSCAN: ";
-k=4;
-l=abs(cr[k]-cr[k-1]);
-r=abs(cr[k+1]-cr[k]);
-if(l>r)
-    td=(cr[k-1])+(199-cr[k]);
-else
-    td=(199-cr[k+1])+cr[k];
-cout<<"\tTotal Distance: "<<td;
+    int i,j,k,n,m,sum=0,x,y,h;
+    cout<<"Enter the size of disk\n";
+    cin>>m;
+    cout<<"Enter number of requests\n";
+    cin>>n;
+    cout<<"Enter the requests\n";
 
-cout<<"\n5. LOOK: ";
-k=4;
-l=abs(cr[k]-cr[k-1]);
-r=abs(cr[k+1]-cr[k]);
-if(l<r)
-    td=(cr[k]-cr[0])+(cr[8]-cr[0]);
-else
-    td=(cr[8]-cr[k])+(cr[8]-cr[0]);
-cout<<"\tTotal Distance: "<<td;
-
-cout<<"\n6. CLOOK: ";
-k=4;
-l=abs(cr[k]-cr[k-1]);
-r=abs(cr[k+1]-cr[k]);
-if(l<r)
-    td=(cr[k]-cr[0])+(cr[8]-cr[k+1]);
-else
-    td=(cr[8]-cr[k])+(cr[k-1]-cr[0]);
-cout<<"\tTotal Distance: "<<td;
-cout<<"\n";
-return 0;
+    // creating an array of size n
+    vector <int> a(n);
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++){
+        if(a[i]>m){
+            cout<<"Error, Unknown position "<<a[i]<<"\n";
+            return 0;
+        }
+    }
+    cout<<"Enter the head position\n";
+    cin>>h;
+    
+    // head will be at h at the starting
+    int temp=h;
+    cout<<temp;
+    for(i=0;i<n;i++){
+        cout<<" -> "<<a[i]<<' ';
+        // calculating the difference for the head movement
+        sum+=abs(a[i]-temp);
+        // head is now at the next I/O request
+        temp=a[i];
+    }
+    cout<<'\n';
+    cout<<"Total head movements = "<< sum<<'\n';
+    return 0;
 }
-
-
